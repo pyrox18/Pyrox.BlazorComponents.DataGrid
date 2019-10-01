5510c905eb3919bd7dab4687eb392d9f75fdca7e
Haryz Izzudin <haryzizzudin@gmail.com>
2019-10-01 18:23:47 +0800

    Add Blazor server E2E test project

---
+++ tests/Pyrox.BlazorComponents.DataGrid.E2ETests/Data/WeatherForecastSortKey.cs
@@ -0,0 +1,10 @@
+﻿namespace Pyrox.BlazorComponents.DataGrid.E2ETests.Data
+{
+    public enum WeatherForecastSortKey : byte
+    {
+        Date,
+        TemperatureC,
+        TemperatureF,
+        Summary
+    }
+}
