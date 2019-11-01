7406c708c1c137990c7cd1c4a0936d6dd1238e2d
Haryz Izzudin <haryzizzudin@gmail.com>
2019-11-01 15:19:28 +0800

    Add sort key display name customisation

---
+++ src/Pyrox.BlazorComponents.DataGrid/Attributes/SortKeyDisplayNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pyrox.BlazorComponents.DataGrid.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SortKeyDisplayNameAttribute : Attribute
+    {
+        public string DisplayName { get; }
+
+        public SortKeyDisplayNameAttribute(string displayName)
+        {
+            DisplayName = displayName;
+        }
+    }
+}
